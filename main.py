a322d5f1a1b5f7d142c33ccd91bad35f521cb034
Pablo Perez <42857634+tripleP16@users.noreply.github.com>
2022-10-17 21:31:01 -0400

    agregando argumento

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from math import prod
 from scrapping.getsite import get_site
 from scrapping.getproducts import get_pages, get_products
 from data.connection import Connection
+import argparse
 
 def insertInBd(products, connection):
     for product in products: 
         connection.insert(product.name, product.stars, product.id)
 
-def main(): 
+def main(min_stars): 
     driver = get_site('https://webscraper.io/test-sites/e-commerce/static/computers/laptops')
     max_page = get_pages(driver, "page-link")
-    products = get_products(max_page,2, driver)
+    products = get_products(max_page,min_stars, driver)
     connection = Connection()
     connection.create_db_tables()
     insertInBd(products, connection)
@@ -19,4 +20,8 @@ def main():
 
 
 if __name__ == "__main__": 
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--stars", type=int, default=3)
+    args = parser.parse_args()
+    min_stars = args.stars
+    main(min_stars)
